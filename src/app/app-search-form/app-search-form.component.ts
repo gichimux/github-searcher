3c3c602776e18bf542c3a07c928c5c8c8fb4a138
gichimux <gichimumwai1@gmail.com>
2019-04-01 01:07:41 +0300

    made bootstrap search field

---
+++ src/app/app-search-form/app-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-search-form',
+  templateUrl: './app-search-form.component.html',
+  styleUrls: ['./app-search-form.component.css']
+})
+export class AppSearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
